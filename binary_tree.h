9119fc2f7d010eeeb7f43b6a0f1ef6bbcc5c8815
MossoMm <149458906+MossoMm@users.noreply.github.com>
2026-05-25 00:16:38 +0700

    Create binary_tree.h

---
+++ binary_tree.h
@@ -0,0 +1,32 @@
+#ifndef TREE_H
+#define TREE_H
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val);
+};
+
+class BinaryTree {
+public:
+    BinaryTree();
+    ~BinaryTree();
+    
+    void insert(int value);
+    bool search(int value);
+    bool remove(int value);
+    int size() const;
+    void clear();
+    
+private:
+    TreeNode* root;
+    int count;
+    
+    TreeNode* insertRec(TreeNode* node, int value);
+    TreeNode* removeRec(TreeNode* node, int value);
+    TreeNode* findMin(TreeNode* node);
+    void clearRec(TreeNode* node);
+};
+
+#endif
